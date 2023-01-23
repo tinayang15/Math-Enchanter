b8044ee50acd8ddca8860f5b5e8256c2b129a666
Tina Yang <Tinayangers@gmail.com>
2023-01-23 15:10:57 -0500

    Checkwin function and submitButton added

--- firstlevel.js
+++ firstlevel.js
@@ -1,29 +1,61 @@
 //variables
 let spaceTwo = document.querySelector('#two')
+let spaceFive = document.querySelector('#five')
 let levelOnescore = document.querySelector('#levelOneScore')
 const resetButton = document.querySelector('.resetButton')
-const nextLevel = document.querySelector('.nextLevel')
+const nextLevel = document.querySelector('.nextButton')
 let declareWinner = false
-
-
+let inputValue = document.getElementById('answer')
+let scoreDisplay = document.getElementById('levelOneScore')
+const submitButton = document.querySelector('.submit')
+let score = 0
 //make randomized number function math.floor and add it to the spaces id 2 and 6
-function randomNumbers () {
-    return Math.floor(Math.random()*9)+0;
-} 
-spaceTwo.innerText = randomNumbers(0,9);
-//innerHTML
+function randomNumbers() {
+    return Math.floor(Math.random() * 9) + 0;
+}
 
-
-// add point to score - have to answer 5 correct to go to next level
-
-// winning condition
-    //add invalid results alert too
-    //compare input value to winning condition
-
+spaceTwo.innerText = randomNumbers()
+spaceFive.innerText = randomNumbers()
+let sum = (parseInt(spaceTwo.innerText) + parseInt(spaceFive.innerText))
+//innerText not innerHTML
 
 //reset button - check if button works
 resetButton.addEventListener('click', () => resetGame());
 
-function resetGame () {
+function resetGame() {
     location.reload()
 }
+
+//winning condition
+// I want this loop to keep running until they score 5 points
+// let userInput = parseInt(inputValue.value)
+
+submitButton.addEventListener('click', () => {
+    checkWin()
+
+})
+//Check score function
+
+const checkScore = () => {
+
+
+}
+// Check win function
+
+const checkWin = () => {
+    if (parseInt(inputValue.value) === sum) {
+        score += 1;
+        checkScore();
+        scoreDisplay.innerText = score;
+        spaceTwo.innerText = randomNumbers();
+        spaceFive.innerText = randomNumbers();
+        sum = (parseInt(spaceTwo.innerText) + parseInt(spaceFive.innerText))
+        inputValue.value = '';
+    } else {
+        inputValue.value = '';
+        alert('Wrong answer, please try again')
+    }
+}
+
+
+
